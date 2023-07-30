ae73f1cfa310de1f860f617f57962fd8bbe5e67d
Justin O'Connor <jsoconno@gmail.com>
2023-07-29 23:30:51 -0400

    try moving basePath and assetPrefix to images object

--- next.config.js
+++ next.config.js
@@ -3,9 +3,9 @@ const nextConfig = {
     output: "export",
     images: {
         unoptimized: true,
+        basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+        assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
     },
-    // basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-    assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
 }
 
 module.exports = nextConfig
