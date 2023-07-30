15770c849a4ce12c1dbafe1f0cdd79b5ef765ed0
Reilly O'Donnell <odonnell.reilly.j@gmail.com>
2023-07-30 15:29:51 -0400

    One more

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Azure Resource Naming Convention Periodic Table',
+  title: 'Periodic Table',
   description: 'Azure Resource Naming Convention Periodic Table',
 };
 
