dcfeb3f4d1a2038dae405c87e73d930a17b07ee6
Justin O'Connor <jsoconno@gmail.com>
2023-07-29 23:32:21 -0400

    revert and push

--- next.config.js
+++ next.config.js
@@ -3,9 +3,10 @@ const nextConfig = {
     output: "export",
     images: {
         unoptimized: true,
-        basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-        assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
+        loader: "imgix",
+        path: "https://onwardplatforms.github.io",
     },
+    assetPrefix: "/azure-periodic-table/",
 }
 
 module.exports = nextConfig
