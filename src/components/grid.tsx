4edee5ec9d2cf3d84bf2f6048f518961a26d83a5
Reilly O'Donnell <odonnell.reilly.j@gmail.com>
2023-08-01 17:26:33 -0400

    Wrong place

--- src/components/grid.tsx
+++ src/components/grid.tsx
@@ -7,7 +7,6 @@ import type { Item } from '@/app/data';
 import { useLayoutEffect, useRef } from 'react';
 import useMobile from '@/custom-hooks/use-mobile';
 import { prefix } from '@/prefix';
-import { handleGoogleTag } from '@/lib/utils';
 
 interface CellProps {
   item: Item;
@@ -71,6 +70,21 @@ const Cell: React.FC<CellProps> = ({
       });
     }
   }, [activeCategory, item.category, ref, isMobile]);
+
+  function handleGoogleTag(item: Item) {
+    if (process.env.NODE_ENV !== 'production') return;
+    // check if window is valid
+    if (typeof window === 'undefined') return;
+
+    // Check if the gtag function is defined
+    if (window.gtag) {
+      window.gtag('event', 'element_selected', {
+        event_category: item.category,
+        event_label: 'resource',
+        event_value: item.name,
+      });
+    }
+  }
 
   return (
     <div
